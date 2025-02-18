d8851a808446f13c520ee0e950122de0a36b03dd
Alexsander <alexsander1996@yahoo.com.br>
2025-02-18 20:41:30 -0300

    integracao com mongo e ativos
    - Criado os métodos que serão usados na API REST dos ativos;
    - Integrado o Mongo ao projeto através do Mongoose;
    - Criado a entidade dos Assets para manuseio no banco;

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,32 @@
-export class Asset {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import crypto from 'crypto';
+import { HydratedDocument } from 'mongoose';
+
+export type AssetDocument = HydratedDocument<Asset>;
+
+@Schema({ timestamps: true }) // com isso o mongoose interpreta a collection e define os campos de criacao e atualizacao
+export class Asset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ unique: true, index: true })
+  name: string;
+
+  @Prop({ unique: true, index: true })
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: number;
+
+  // exclamacao indica apenas a modelagem dos dados, nao gerara em compilacao
+  @Prop()
+  createdAt!: Date;
+
+  @Prop()
+  updatedAt!: Date;
+}
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
