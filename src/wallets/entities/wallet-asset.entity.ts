2fbdb8c025c3c1f95953a364a7fc69b9cd295994
Alexsander <alexsander1996@yahoo.com.br>
2025-02-18 22:51:59 -0300

    criacao relacionamento wallet asset
    - Criado a entidade WalletAsset para que possamos relacionar os ativos de uma carteira;
    - Criado rota para inserção de um ativo na carteira;

---
+++ src/wallets/entities/wallet-asset.entity.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import crypto from 'crypto';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Wallet, WalletDocument } from './wallet.entity';
+import { Asset, AssetDocument } from 'src/assets/entities/asset.entity';
+
+export type WalletAssetDocument = HydratedDocument<WalletAsset>;
+
+@Schema({ timestamps: true }) // com isso o mongoose interpreta a collection e define os campos de criacao e atualizacao
+export class WalletAsset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  shares: number;
+
+  @Prop({ type: String, ref: Wallet.name }) // adicao do ref para que o mongoose saiba referenciar a relacao
+  wallet: WalletDocument | string;
+
+  @Prop({ type: String, ref: Asset.name }) // adicao do ref para que o mongoose saiba referenciar a relacao
+  asset: AssetDocument | string;
+
+  // exclamacao indica apenas a modelagem dos dados, nao gerara em compilacao
+  @Prop()
+  createdAt!: Date;
+
+  @Prop()
+  updatedAt!: Date;
+}
+
+export const WalletAssetSchema = SchemaFactory.createForClass(WalletAsset);
+
+// criacao de um index para evitar duplicacoes
+WalletAssetSchema.index({ wallet: 1, asset: 1 }, { unique: true });
