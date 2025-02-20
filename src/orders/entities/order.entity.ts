64720fc5d85bae72026adc4c7494de8b0e570f7e
Alexsander <alexsander1996@yahoo.com.br>
2025-02-20 20:43:58 -0300

    implementacao das ordens
    - Criação da entidade das Ordens;
    - Implementação das rotas das ordens;
    - Criação do DTO das ordens;
    - Atualização da collection do insomnia com os endpoints;

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,55 @@
-export class Order {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import crypto from 'crypto';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Asset, AssetDocument } from 'src/assets/entities/asset.entity';
+import { Wallet, WalletDocument } from 'src/wallets/entities/wallet.entity';
+
+export type OrderDocument = HydratedDocument<Order>;
+
+export enum OrderType {
+  BUY = 'BUY',
+  SELL = 'SELL',
+}
+
+export enum OrderStatus {
+  PENDING = 'PENDING',
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+  FAILED = 'FAILED',
+}
+
+@Schema({ timestamps: true }) // com isso o mongoose interpreta a collection e define os campos de criacao e atualizacao
+export class Order {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  shares: number;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  partial: number;
+
+  @Prop()
+  price: number;
+
+  @Prop({ type: String, ref: Wallet.name }) // adicao do ref para que o mongoose saiba referenciar a relacao
+  wallet: WalletDocument | string;
+
+  @Prop({ type: String, ref: Asset.name }) // adicao do ref para que o mongoose saiba referenciar a relacao
+  asset: AssetDocument | string;
+
+  @Prop()
+  type: OrderType;
+
+  @Prop()
+  status: OrderStatus;
+
+  // exclamacao indica apenas a modelagem dos dados, nao gerara em compilacao
+  @Prop()
+  createdAt!: Date;
+
+  @Prop()
+  updatedAt!: Date;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
